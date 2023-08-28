6a65b3f99c5ca5aeea4e3095cca3e754700a1833
Patrick Wu <a10115101@gmail.com>
2023-08-28 18:24:35 +0800

    add profile friends page

--- frontend/src/features/profile/ProfileFriends.jsx
+++ frontend/src/features/profile/ProfileFriends.jsx
@@ -1,5 +1,113 @@
+import styles from "./ProfileFriends.module.css";
+
 function ProfileFriends() {
-  return <div>ProfileFriends</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.topContainer}>
+        <div className={styles.topContainerLeft}>
+          <button>All</button>
+          <button>Pending</button>
+        </div>
+        <div className={styles.topContainerRight}>
+          <i className="fa-solid fa-magnifying-glass" />
+          <input type="text" placeholder="Search for friend" />
+        </div>
+      </div>
+
+      <div className={styles.bottomContainer}>
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+
+        <div className={styles.cardContainer}>
+          <div className={styles.cardContainerLeft}></div>
+          <div className={styles.cardContainerCenter}>
+            <div>Name: Patricddwdedwek</div>
+            <div>Coutry: USfdsfsdfdA</div>
+          </div>
+          <div className={styles.cardContainerRight}>
+            <button>Accept</button>
+            <button>Reject</button>
+            <button>Remove</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileFriends;
