c81904f7238813f1fdfc7981bb4d628a29a69495
Patrick Wu <a10115101@gmail.com>
2023-08-28 15:12:08 +0800

    add profile detail page

--- frontend/src/features/profile/ProfileDetails.jsx
+++ frontend/src/features/profile/ProfileDetails.jsx
@@ -3,7 +3,29 @@ import styles from "./ProfileDetails.module.css";
 function ProfileDetails() {
   return (
     <div className={styles.container}>
-      <div>1</div>
+      <div className={styles.data}>
+        <div>
+          <span>
+            <i className="fa-regular fa-flag" />
+            Visited Countries Counts : 100
+          </span>
+        </div>
+        <div>
+          <span>
+            <i className="fa-solid fa-city" />
+            Visited Cities Counts : 100
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.chart}>
+        <div>
+          <div>bar chart</div>
+        </div>
+        <div>
+          <div>pie chart</div>
+        </div>
+      </div>
     </div>
   );
 }
