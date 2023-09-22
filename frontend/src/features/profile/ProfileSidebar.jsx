0a27331a028b8b2f47f29f9aa6be769a0d6c292b
Patrick Wu <a10115101@gmail.com>
2023-09-22 16:40:36 +0800

    refactor profile sidebar css

--- frontend/src/features/profile/ProfileSidebar.jsx
+++ frontend/src/features/profile/ProfileSidebar.jsx
@@ -1,31 +1,42 @@
 import { Link } from "react-router-dom";
 
+import { getUser } from "src/services/apiAuth";
 import styles from "./ProfileSidebar.module.css";
 
 function ProfileSidebar() {
+  const userInfo = getUser().user;
+
   return (
     <div className={styles.container}>
-      <ul>
-        <div>人物頭像&名稱</div>
-        <li>
-          <Link to="/profile/about">
-            <i className="fa-solid fa-id-badge" />
-            About Me
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile/statis">
-            <i className="fa-solid fa-chart-line" />
-            Statis
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile/friends">
-            <i className="fa-solid fa-user-group" />
-            Friends
-          </Link>
-        </li>
-      </ul>
+      <div className={styles.topContainer}>
+        <img
+          src={`http://localhost:3000/public/users/${userInfo.photo}`}
+          alt="pic"
+        />
+        <p>{userInfo.username && userInfo.username}</p>
+      </div>
+      <div className={styles.bottomContainer}>
+        <ul>
+          <li>
+            <Link to="/profile/about">
+              <i className="fa-solid fa-id-badge" />
+              <span> About Me</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile/statis">
+              <i className="fa-solid fa-chart-line" />
+              <span>Statis</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile/friends">
+              <i className="fa-solid fa-user-group" />
+              <span>Friends</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
